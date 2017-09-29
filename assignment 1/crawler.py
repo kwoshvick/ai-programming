9df3fd5f50b4496287c16843a12ce05261cf8f87
kwoshvick <kwoshvick@gmail.com>
2017-09-29 09:00:14 +0300

    used firts principles

--- assignment 1/crawler.py
+++ assignment 1/crawler.py
@@ -1,13 +1,20 @@
-import urllib.request as rq
-from bs4 import BeautifulSoup
-
-url = input('Enter url (start https or http) : ')
-
-
-html = rq.urlopen(url).read()
-soup = BeautifulSoup(html,"lxml")
-
-tags = soup('a')
-
-for tag in tags:
-    print(tag.get('href',None))
+import urllib
+test = urllib.urlopen("http://strathmore.edu/").read()
+sane = 0
+needlestack = []
+while sane == 0:
+  curpos = test.find("href")
+  if curpos >= 0:
+    testlen = len(test)
+    test = test[curpos:testlen]
+    curpos = test.find('"')
+    testlen = len(test)
+    test = test[curpos+1:testlen]
+    curpos = test.find('"')
+    needle = test[0:curpos]
+    if needle.startswith("http" or "www"):
+        needlestack.append(needle)
+  else:
+    sane = 1
+for item in needlestack:
+    print item
