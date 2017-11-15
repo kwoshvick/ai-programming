5211506b516dfe410af8dce0c450f758e54c5017
kwoshvick <kwoshvick@gmail.com>
2017-11-15 15:58:10 +0300

    use of regex

--- assignment 4/bfs.py
+++ assignment 4/bfs.py
@@ -1,39 +1,46 @@
 import urllib.request as rq
-from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
+import re
+from collections import OrderedDict
 
-# url = input('Enter link ')
-# repeat = input('Enter breadth ')
+
 
 wordsDictionary = dict()
-
 urllist = list()
+mostfrequent = OrderedDict()
 
 def getLinks(link):
     html = rq.urlopen(link).read()
-    soup = BeautifulSoup(html,"lxml")
-    tags = soup('a')
-    for tag in tags:
-        urllist.append(tag.get('href'))
+    html = str(html)
+    urls = re.findall(r'href=[\'"]?([^\'" >]+)', html)
+    i = ' '.join(urls)
+    links = i.split()
+    for link in links:
+        if link == '\\,':
+            continue
+        if link == '\\':
+            continue
+        urllist.append(link)
 
-def get_p_tags(link):
-    html = rq.urlopen(link).read()
-    soup = BeautifulSoup(html, "lxml")
-    tags = soup.findAll('p')
-    for tag in tags:
-        p = tag.text
-        m = p.strip('. : / = ')
-        m = m.lower()
-        append_words(m.split())
+def get_p_tags(urls):
+    for link in urls:
+        try:
+            html = rq.urlopen(link).read()
+            html = str(html)
+            m = re.findall(r'<p>([^]]+)</p>', html)
+            for i in m:
+                n = i.strip('. <p> </p> ')
+                for y in n.split():
+                    y2 = y.lower()
+                    wordsDictionary[y2] = wordsDictionary.get(y2, 0) + 1
+        except :
+            continue
 
-def append_words(words):
-    for word in words:
-        wordsDictionary[word] = wordsDictionary.get(word, 0) + 1
 
 
 def plot_graph(wordsDictionary):
     average = sum(wordsDictionary.values())/len(wordsDictionary.values())
-    frequentWords = {k: v for (k, v) in wordsDictionary.items() if v > average}
+    frequentWords = {k: v for (k, v) in wordsDictionary.items() if v > average-1}
     plt.bar(range(len(frequentWords)), frequentWords.values())
     plt.xticks(range(len(frequentWords)), frequentWords.keys(), rotation=-60)
     plt.title('Histogram of Frequency of words')
@@ -43,7 +50,6 @@ def plot_graph(wordsDictionary):
 
 
 
-
-# get_p_tags('http://www.strathmore.edu/')
-get_p_tags('http://www.strathmore.edu/news/student-spotlight-akilimali-yasin-strathmore-has-exposed-me-to-many-prospects/')
+getLinks('http://www.strathmore.edu/')
+get_p_tags(urllist)
 plot_graph(wordsDictionary)
