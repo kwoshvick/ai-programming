3c134348dbb15387e7b54bbc2ae2fb43e45f2041
kwoshvick <kwoshvick@gmail.com>
2017-11-15 12:14:36 +0300

    plot graph

--- assignment 4/bfs.py
+++ assignment 4/bfs.py
@@ -1,24 +1,53 @@
 import urllib.request as rq
 from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
 
-url = input('Enter link ')
+# url = input('Enter link ')
 # repeat = input('Enter breadth ')
 
-
+wordsDictionary = dict()
 
 urllist = list()
-last_link = list()
 
 def getLinks(link):
-    global soup
-    global tags
     html = rq.urlopen(link).read()
     soup = BeautifulSoup(html,"lxml")
     tags = soup('a')
     for tag in tags:
         urllist.append(tag.get('href'))
 
+def get_p_tags(link):
+    html = rq.urlopen(link).read()
+    soup = BeautifulSoup(html, "lxml")
+    tags = soup.findAll('p')
+    for tag in tags:
+        p = tag.text
+        # print(type(p))
+        m = p.strip('. : / = ')
+        m = m.lower()
+        # print(m.split())
+        append_words(m.split())
 
-getLinks(url)
+def append_words(words):
+    for word in words:
+        wordsDictionary[word] = wordsDictionary.get(word, 0) + 1
 
-print(urllist)
+
+def plot_graph(wordsDictionary):
+    frequentWords = {k: v for (k, v) in wordsDictionary.items() if v > 0}
+    plt.bar(range(len(frequentWords)), frequentWords.values())
+    plt.xticks(range(len(frequentWords)), frequentWords.keys(), rotation=-60)
+    plt.title('Histogram of Frequency of words')
+    plt.xlabel('Words')
+    plt.ylabel('Frequency')
+    plt.show()
+
+
+
+
+get_p_tags('http://www.strathmore.edu/')
+plot_graph(wordsDictionary)
+
+# print(wordsDictionary)
+#
+# print(urllist)
